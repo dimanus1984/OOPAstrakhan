90bac7a00893fb55d68edc83ccd0d582dc238023
DumaHycc <nuss_dmitriy@mail.ru>
2021-06-27 21:24:31 +0400

    commit

--- String/main.cpp
+++ String/main.cpp
@@ -7,124 +7,154 @@ using std::endl;
 #define tab "\t"
 #define delimiter "\n--------------------------------------------------------------\n"
 
+////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////  CLASS DECLARATION - ОБЪЯВЛЕНИЕ КЛАССА  ////////////////////////////////
+
 class String;
 String operator+(const String& left, const String& right);
 
 class String
 {
-	unsigned int size;	//Размер строки в байтах
-	char* str;			//
+	unsigned int size;	//Размер строки в байтах.
+	char* str;			//Указатель на строку в динамической памяти.
 public:
-	unsigned int get_size()const
-	{
-		return size;
-	}
-	const char* get_str()const
-	{
-		return str;
-	}
-	char* get_str()
-	{
-		return str;
-	}
+	unsigned int get_size()const;
+	const char* get_str()const;
+	char* get_str();
 
 	//			Constructors:
-	explicit String(unsigned int size = 80) :size(size), str(new char[size] {})
-	{
-		//this->size = size;
-		//this->str = new char[size] {};
-		cout << "SizeConstructor:\t" << this << endl;
-	}
-
-	String(const char str[]) :String(strlen(str) + 1)
-	{
-		for (int i = 0; str[i]; i++)
-			this->str[i] = str[i];
-		cout << "Constructor:\t\t" << this << endl;
-	}
-
-	String(const String& other) :String(other.str)
-	{
-		//this->size = other.size;
-		//this->str = other.str;	//Низя так делать с указателями!!! Shallow copy
-		//this->str = new char[size] {};
-		//for (int i = 0; i < size; i++)
-			//this->str[i] = other.str[i];	//Побитовое (поэдементное, побайтовое) копирование
-											//Deep copy.
-		cout << "CopyConstructor:\t" << this << endl;
-	}
-
-	String(String&& other)
-	{
-		//MoveConstructor должен работать так, как НЕ должен работать CopyConstructor, то есть,
-		//MoveConstructor должен выполнять DeepCopy
-		//MoveConstructor должен выполнять ShallowCopy,
-		//то естоь копировать только адрес на уже выделенную память
-		//MoveConstructor НЕ ДОЛЖЕН ВЫДЕЛЯТЬ ДИНАМИЧЕСКУЮ ПАМЯТЬ!!!
-		//он берет память временного безымянного объекта, и передает ее создаваемому объекту.
-		//При этом, временный объект должен потерять доступ к своему значению.
-		this->size = other.size;
-		this->str = other.str;
-		other.str = nullptr;
-		//чтобы не удалял память, которую мы у него взяли? конструктор переноса должен занулить указатель
-		//на ту память, которую мы переносим, вот поэтому и принимается не константная двойная ссылка на объект нашего класса.
-		cout << "MoveConstructor:\t" << this << endl;
-	}
-
-
-	~String()
-	{
-		delete[] this->str;
-		cout << "Destructor:\t\t" << this << endl;
-	}
+	explicit String(unsigned int size = 80);
+	String(const char str[]);
+	String(const String& other);
+	String(String&& other);
+	~String();
 
 	//			Operators:
-	String& operator=(const String& other)
-	{
-		if (this == &other)return *this;
-		//1) Удаляем старое значение объекта:
-		delete[] this->str;
-		//2) Выполняем копирование:
-		this->size = other.size;		//Копируем размер
-		this->str = new char[size] {};	//Выделяем память
-		for (int i = 0; i < size; i++)
-			this->str[i] = other.str[i];
-		cout << "CopyAssignment:\t\t" << this << endl;
-		return *this;
-	}
-
-	String& operator=(String&& other)	//Этот объект на момент вызова уже занимает память.
-	{
-		//Удаляем ту динамическую память, которую мы принимаем сейчас
-		delete[] this->str;
-		this->size = other.size;
-		this->str = other.str;
-		other.str = nullptr;	//Зануляем, чтобы конструктор его не удалил.
-		cout << "MoveAssignment:\t\t" << this << endl;
-		return *this;
-	}
-
-	String& operator+=(const String& other)
-	{
-		return *this = *this + other;
-	}
-
-	const char& operator[](unsigned int i)const
-	{
-		return str[i];
-	}
-	char& operator[](unsigned int i)
-	{
-		return str[i];
-	}
+	String& operator=(const String& other);
+	String& operator=(String&& other);	//Этот объект на момент вызова уже занимает память.
+	String& operator+=(const String& other);
+	const char& operator[](unsigned int i)const;
+	char& operator[](unsigned int i);
 
 	//			Methods:
-	void print()const
-	{
-		cout << "Size:\t" << size << endl;
-		cout << "Str:\t" << str << endl;
-	}
+	void print()const;
 };
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////  CLASS DECLARATION END  /////////////////////////////////////////
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////  CLASS DEFINITION - ОПРЕДЕЛЕНИЕ КЛАССА  /////////////////////////////////
+
+unsigned int String::get_size()const
+{
+	return size;
+}
+const char* String::get_str()const
+{
+	return str;
+}
+char* String::get_str()
+{
+	return str;
+}
+
+//			Constructors:
+explicit String::String(unsigned int size = 80) :size(size), str(new char[size] {})
+{
+	//this->size = size;
+	//this->str = new char[size] {};
+	cout << "SizeConstructor:\t" << this << endl;
+}
+
+String::String(const char str[]) :String(strlen(str) + 1)
+{
+	for (int i = 0; str[i]; i++)
+		this->str[i] = str[i];
+	cout << "Constructor:\t\t" << this << endl;
+}
+
+String::String(const String& other) :String(other.str)
+{
+	//this->size = other.size;
+	//this->str = other.str;	//Низя так делать с указателями!!! Shallow copy
+	//this->str = new char[size] {};
+	//for (int i = 0; i < size; i++)
+		//this->str[i] = other.str[i];	//Побитовое (поэдементное, побайтовое) копирование
+										//Deep copy.
+	cout << "CopyConstructor:\t" << this << endl;
+}
+
+String::String(String&& other)
+{
+	//MoveConstructor должен работать так, как НЕ должен работать CopyConstructor, то есть,
+	//MoveConstructor должен выполнять DeepCopy
+	//MoveConstructor должен выполнять ShallowCopy,
+	//то естоь копировать только адрес на уже выделенную память
+	//MoveConstructor НЕ ДОЛЖЕН ВЫДЕЛЯТЬ ДИНАМИЧЕСКУЮ ПАМЯТЬ!!!
+	//он берет память временного безымянного объекта, и передает ее создаваемому объекту.
+	//При этом, временный объект должен потерять доступ к своему значению.
+	this->size = other.size;
+	this->str = other.str;
+	other.str = nullptr;
+	//чтобы не удалял память, которую мы у него взяли? конструктор переноса должен занулить указатель
+	//на ту память, которую мы переносим, вот поэтому и принимается не константная двойная ссылка на объект нашего класса.
+	cout << "MoveConstructor:\t" << this << endl;
+}
+
+
+String::~String()
+{
+	delete[] this->str;
+	cout << "Destructor:\t\t" << this << endl;
+}
+
+//			Operators:
+String& String::operator=(const String& other)
+{
+	if (this == &other)return *this;
+	//1) Удаляем старое значение объекта:
+	delete[] this->str;
+	//2) Выполняем копирование:
+	this->size = other.size;		//Копируем размер
+	this->str = new char[size] {};	//Выделяем память
+	for (int i = 0; i < size; i++)
+		this->str[i] = other.str[i];
+	cout << "CopyAssignment:\t\t" << this << endl;
+	return *this;
+}
+
+String& String::operator=(String&& other)	//Этот объект на момент вызова уже занимает память.
+{
+	//Удаляем ту динамическую память, которую мы принимаем сейчас
+	delete[] this->str;
+	this->size = other.size;
+	this->str = other.str;
+	other.str = nullptr;	//Зануляем, чтобы конструктор его не удалил.
+	cout << "MoveAssignment:\t\t" << this << endl;
+	return *this;
+}
+
+String& String::operator+=(const String& other)
+{
+	return *this = *this + other;
+}
+
+const char& String::operator[](unsigned int i)const
+{
+	return str[i];
+}
+char& String::operator[](unsigned int i)
+{
+	return str[i];
+}
+
+//			Methods:
+void String::print()const
+{
+	cout << "Size:\t" << size << endl;
+	cout << "Str:\t" << str << endl;
+}
 
 String operator+(const String& left, const String& right)
 {
@@ -148,6 +178,9 @@ std::ostream& operator<<(std::ostream& os, const String& obj)
 //{
 //	return is >> obj.get_str();
 //}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////  CLASS DEFINITION END (КОНЕЦ ОПРЕДЕЛЕНИЯ КЛАССА) ///////////////////////////
 
 //#define CONSTRUCTORS_CHECK
 //#define INPUT_CHECK
